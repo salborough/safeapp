5450bb40352336cb616720073bbebc1275ededa1
Sean Alborough <salborough@gmail.com>
2013-11-16 22:52:23 +0200

    changed base url config setting

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,8 @@
 | path to your installation.
 |
 */
-$config['base_url']	= "http://localhost/safeCI";
+//$config['base_url']	= "http://localhost/safeCI";
+$config['base_url']	= "http://safeappdev.alboroughlabs.com";
 
 /*
 |--------------------------------------------------------------------------
